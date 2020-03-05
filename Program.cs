1051a188cf549088a62ec7d54d860de6b813d846
Dana Marcuse <dana@marcuse.us>
2020-03-05 17:55:03 -0500

    Fix Windows crash and improve README

--- Program.cs
+++ Program.cs
@@ -4,42 +4,44 @@ using Mono.Cecil.Cil;
 using System.IO;
 
 namespace CelesteUncapped {
-  internal class Program {
-    public static void Main(string[] args) {
-      if (args.Length < 1) {
-        System.Console.WriteLine("Missing argument!\nUsage: CelesteUncapped.exe <Celeste.exe path>");
-        return;
-      }
+	internal class Program {
+		public static void Main(string[] args) {
+			if (args.Length < 1) {
+				System.Console.WriteLine("Missing argument!\nUsage: CelesteUncapped.exe <Celeste.exe path>");
+				return;
+			}
 
-      // get main game entry point
-      var game = AssemblyDefinition.ReadAssembly(args[0]).MainModule;
-      var body = game.Types.Single(t => t.Name == "Celeste")
-        .Methods.Single(m => m.IsConstructor && !m.IsStatic).Body;
+			using (var game = AssemblyDefinition.ReadAssembly(args[0], new ReaderParameters {ReadWrite = true})) {
+				// get main game entry point
+				var body = game.MainModule.Types.Single(t => t.Name == "Celeste")
+					.Methods.Single(m => m.IsConstructor && !m.IsStatic).Body;
 
-      // find set_IsFixedTimeStep call
-      var b = body.Instructions
-        .Single(i => i.OpCode == OpCodes.Call && ((MethodReference) i.Operand).Name == "set_IsFixedTimeStep")
-        .Previous;
-      if (b.OpCode != OpCodes.Ldc_I4_1) {
-        System.Console.WriteLine($"Error! Expected ldc.i4.1, got {b.OpCode}");
-        System.Console.WriteLine("Not patching game: Are you sure the game isn't already patched?");
-        return;
-      }
+				// find set_IsFixedTimeStep call
+				var b = body.Instructions
+					.Single(i =>
+						i.OpCode == OpCodes.Call && ((MethodReference) i.Operand).Name == "set_IsFixedTimeStep")
+					.Previous;
 
-      // update to set to false instead of true
-      var il = body.GetILProcessor();
-      il.Replace(b, il.Create(OpCodes.Ldc_I4_0));
+				if (b.OpCode != OpCodes.Ldc_I4_1) {
+					System.Console.WriteLine($"Error! Expected ldc.i4.1, got {b.OpCode}");
+					System.Console.WriteLine("Not patching game: Are you sure the game isn't already patched?");
+					return;
+				}
 
-      // save the patched game
-      var oldPath = args[0] + ".old";
-      var tmpPath = args[0] + ".tmp";
-      game.Write(tmpPath);
-      if (File.Exists(oldPath)) {
-        File.Delete(oldPath);
-      }
-      File.Move(args[0], oldPath);
-      File.Move(tmpPath, args[0]);
-      System.Console.WriteLine("Game patched! Old executable moved to " + oldPath);
-    }
-  }
+				// update to set to false instead of true
+				var il = body.GetILProcessor();
+				il.Replace(b, il.Create(OpCodes.Ldc_I4_0));
+
+				// save the patched game
+				var oldPath = args[0] + ".old";
+				if (File.Exists(oldPath)) {
+					File.Delete(oldPath);
+				}
+
+				File.Copy(args[0], oldPath);
+				game.Write();
+				System.Console.WriteLine("Game patched! Old executable moved to " + oldPath);
+			}
+		}
+	}
 }
--- README.md
+++ README.md
@@ -6,9 +6,14 @@ A tool to patch Celeste to remove the framerate cap
 
 Download the [latest release](/../../releases/latest), then run it in a terminal, passing the path to Celeste.exe.
 Make sure the game is closed before running the tool.
+You'll also probably want to enable vsync to limit the game's framerate to that of your monitor.
 
 Once patched, the program doesn't need to be rerun unless the game is updated or redownloaded.
 
-## Linux example
+**Windows**
+
+`CelesteUncapped.exe "C:\Program Files (x86)\Steam\steamapps\common\Celeste\Celeste.exe"`
+
+**Linux**
 
 `mono CelesteUncapped.exe ~/.local/share/Steam/steamapps/common/Celeste/Celeste.exe`
